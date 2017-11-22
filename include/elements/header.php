a72c7f45b3517bb695acf79ac0d4892ff987162b
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-11-22 17:42:14 +0100

    Affichage selon conexion ou non

--- include/elements/header.php
+++ include/elements/header.php
@@ -4,9 +4,15 @@
 
 	<form id="search-term" class="d-inline-block form-inline mt-2 mt-md-0">
 		<input id="query" class="form-control mr-sm-2" type="text" placeholder="Rechercher">
-		<button role="button" class="btn btn-outline-secondary my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+		<button role="button" class="btn btn-outline-secondary my-2 my-sm-0" type="submit">
+			<i class="fa fa-search" aria-hidden="true"></i>
+		</button>
 	</form>
-	<button for="inscription" role="button" class="btn btn-success my-2 my-sm-0 btn-user">Inscription</button>
-	<button for="connexion" role="button" class="btn btn-info my-2 my-sm-0 btn-user">Connexion</button>
-	
+	<?php if(!isConnected()): ?>
+		<button for="inscription" role="button" class="btn btn-success my-2 my-sm-0 btn-user">Inscription</button>
+		<button for="connexion" role="button" class="btn btn-info my-2 my-sm-0 btn-user">Connexion</button>
+	<?php else: ?>
+		<h5 class="d-inline mr-3" id="pseudo-display">Bonjour, <i><?= $_SESSION['pseudo']?></i></h5>
+		<button for="deconnexion" role="button" class="btn btn-outline-info my-2 my-sm-0">Se déconnecter</button>
+	<?php endif ?>
 </nav>

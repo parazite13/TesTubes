35c9302b6cd5337611855d533ceb56e0515e791f
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-11-22 17:04:47 +0100

    Inscription connexion

--- js/script.js
+++ js/script.js
@@ -72,6 +72,37 @@ $(document).ready(function () {
 			}
 		});
 	});
+
+	//Boutons connexion et inscription
+	$('.btn-user').click(function(){
+		var html = "";
+		$('#blur').removeClass('d-none');
+		if($(this).attr('for') == 'inscription'){
+			html += '<div id="inscription" class="card p-3" style="position: absolute; z-index:1200; top:10vh; left: 40vw; right: 40vw; width: 20vw;">\
+						<form action="" method="post" onSubmit="return checkInscription();">\
+							<label for="username" class="lead">Nom d\'utilisateur :</label>\
+							<input class="form-control mx-auto" type="text" id="username" name="username" style="width: 100%;"/>\
+							<label for="pwd" class="lead">Mot de passe :</label>\
+							<input class="form-control mx-auto" type="password" id="pwd" name="pwd" style="width: 100%;"/>\
+							<label for="pwd-bis" class="lead">Confirmer mot de passe :</label>\
+							<input class="form-control mx-auto" type="password" id="pwd-bis" name="pwd-bis" style="width: 100%;"/>\
+							<button type="submit" role="button" class="mt-2 btn btn-primary">S\'inscrire</button>\
+						</form>\
+					</div>';
+		}else if($(this).attr('for') == 'connexion'){
+			html += '<div id="connexion" class="card p-3" style="position: absolute; z-index:1200; top:10vh; left: 40vw; right: 40vw; width: 20vw;">\
+						<form action="" method="post" onSubmit="return checkConnexion();">\
+							<label for="username" class="lead">Nom d\'utilisateur :</label>\
+							<input class="form-control mx-auto" type="text" id="username-con" name="username-con" style="width: 100%;"/>\
+							<label for="pwd" class="lead">Mot de passe :</label>\
+							<input class="form-control mx-auto" type="password" id="pwd-con" name="pwd-con" style="width: 100%;"/>\
+							<button type="submit" role="button" class="mt-2 btn btn-primary">Se connecter</button>\
+						</form>\
+					</div>';
+		}
+		$('.overlay').removeClass('d-none');
+		$('.overlay').html(html);
+	});
 });
 
 function initMap(){
@@ -136,6 +167,17 @@ function displayMap(coords, radius){
 	    	});
 		}
 	});
+}
+
+function checkConnexion(){
+	var rgx = /^([a-zA-Z]|\d)+$/;
+	var pseudo = $('#username-con').val();
+	var pwd = $('#pwd-con').val();
+
+	return pseudo.length > 4 && pwd.length > 6 && rgx.test(pseudo) && rgx.test(pwd);
+}
+
+function checkInscription(){
 
 }
 

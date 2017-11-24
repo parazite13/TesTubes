103fd960f57a833d8a2a9446da37ab9e0a8d52dd
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-11-24 18:02:17 +0100

    Checkbox affectent la bdd avec bouton "enregistrer"

--- pages/account/index.php
+++ pages/account/index.php
@@ -10,24 +10,40 @@
 	
 	<?php 
 		getHeader(); 
+
+		//l'utilisateur a cliqué sur le bouton "enregistrer"
+		if (isset($_POST['changePref'])){
+			$query = "	UPDATE `preferences` 
+						SET `titre` = ". (isset($_POST['titre']) ? '1' : '0') ." ,
+						`auteur` = ". (isset($_POST['auteur']) ? '1' : '0') ." ,
+						`vues` = ". (isset($_POST['vues']) ? '1' : '0') ." ,
+						`date` = ". (isset($_POST['date']) ? '1' : '0') ." ,
+						`description` = ". (isset($_POST['description']) ? '1' : '0') ." ,
+						`duree` = ". (isset($_POST['duree']) ? '1' : '0') ." 
+						WHERE `pseudo` = '" . $_SESSION['pseudo']."'";
+			$db->executeQuery($query);
+		}
 	?>
 	<div class="container margin-top">
 		<div class="control-group">
-			<?php
-			$query = "SELECT * FROM `preferences` WHERE `pseudo`='". $_SESSION['pseudo'] ."'";
-			$array = $db->getRowFromQuery($query, false);
-			foreach ($array as $key => $value):
-				if($key != 'id' && $key != 'pseudo'):
-			?>
-				<label class="control control-checkbox">
-					<?= ucfirst($key) ?>
-				    <input type="checkbox" <?php $value ? print 'checked="unchecked"':print ''?> />
-				    <div class="control_indicator"></div>
-				</label>
-			<?php 
-				endif;
-			endforeach;
-			?>
+			<form method="post" action="">
+				<?php
+				$query = "SELECT * FROM `preferences` WHERE `pseudo`='". $_SESSION['pseudo'] ."'";
+				$array = $db->getRowFromQuery($query, false);
+				foreach ($array as $key => $value):
+					if($key != 'id' && $key != 'pseudo'):
+				?>
+						<label class="control control-checkbox">
+							<?= ucfirst($key) ?>
+							<input type="checkbox" <?php echo 'name="'.$key.'"' ?> <?php $value ? print 'checked="unchecked"':print ''?> />
+							<div class="control_indicator"></div>
+						</label>
+				<?php 
+					endif;
+				endforeach;
+				?>
+				<button class="btn btn-success" name="changePref" role="button" type="submit">Enregistrer</button>
+			</form>
 		</div>
 	</div>
 	<?php getFooter(); ?>

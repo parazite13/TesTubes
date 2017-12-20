cdf40cc9d7fe10f1f9f13fc4803aa6a18baa4da7
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-12-20 10:39:33 +0100

    KDO

--- ajax/checkComment.php
+++ ajax/checkComment.php
@@ -6,8 +6,8 @@ require '../init.php';
 $insert = 
 	array(
 		'date' => date("Y-m-d H:i:s"),
-		'id_user' => $_SESSION['id'],
-		'id_video' => $_GET['id_video'],
+		'id_user' => intval($_SESSION['id']),
+		'id_video' => intval($_GET['id_video']),
 		'comment' => $_GET['comment']
 	);	
 

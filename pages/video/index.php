33c4a3ae96c95b9c0a793ba4fbbef7a107390f4f
Vincent <vincent.paoli.1@etu.univ-amu.fr>
2017-12-20 11:05:44 +0100

    Vallu1111

--- pages/video/index.php
+++ pages/video/index.php
@@ -31,8 +31,8 @@
 
 			<?php if(isConnected()):?>
 				<div class="form-group container-fluid">
-					<label for="myComment">Votre commentaire : </label>
-					<textarea class="form-control" id="comment" name="comment" rows="3"></textarea>
+					<label>Votre commentaire : </label>
+					<textarea class="form-control" id="comment" rows="3"></textarea>
 				</div>
 				<button class="btn btn-success" role="button" onclick="checkComment()">Publier</button>
 			<?php endif ?>
@@ -52,24 +52,73 @@
 				</div>
 			</div>
 			<?php endforeach?>
-
 		</div>
 
 		<script type="text/javascript">
+			var chrono;
+			$(document).ready(function(){
+				chrono = new Timer();
+				chrono.run();
+			});
+
 			$(window).bind('beforeunload', function(e){
 	  			$.post("ajax/killProcess.php", {port: $('#video').attr('port')});
 			});
-
 
 			function checkComment(){
 				var url = 'ajax/checkComment.php';
 				var idVideo = '<?=$video?>';
 				var myComment = $('#comment').val();
-				$.get(url, {id_video:idVideo, comment:myComment}, function(results){
+				var timeComment = parseInt(chrono.min) * 60 + parseInt(chrono.sec);
+				$.get(url, {id_video:idVideo, comment:myComment, chrono:timeComment}, function(results){
 					console.log(results);
 				});
 			}
 
+			function Timer(){
+				this.dateStartChrono = new Date();
+				this.end;
+				this.diff;
+				this.min;
+				this.sec;
+				this.msec;
+				this.timerID;
+				this.run = function(){
+					this.end = new Date();
+					this.diff = this.end - this.dateStartChrono;
+					this.diff = new Date(this.diff);
+					this.msec = this.diff.getMilliseconds();
+					this.sec = this.diff.getSeconds();
+					this.min = this.diff.getMinutes();
+					if (this.min < 10){
+						this.min = "0" + this.min;
+					}
+					if (this.sec < 10){
+						this.sec = "0" + this.sec;
+					}
+					if(this.msec < 10){
+						this.msec = "00" +this.msec;
+					}
+					else if(this.msec < 100){
+						this.msec = "0" +this.msec;
+					}
+					this.timerID = setTimeout(this.run.bind(this), 10);
+				}
+				this.reset = function(){
+					clearTimeout(this.timerID);
+					this.min = '00';
+					this.sec = '00';
+					this.msec = '00';
+				}
+				this.pause = function(){
+					clearTimeout(this.timerID);
+				}
+				this.resume = function(){
+					this.dateStartChrono = new Date() - this.diff;
+					this.dateStartChrono = new Date(this.dateStartChrono);
+					this.run();
+				}
+			}
 		</script>
 
 	<?php else: ?>
